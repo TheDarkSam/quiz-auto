c9b678db871bd0e94531a477590af8e9ced16e92
Samuel V <samuel_soadsp@outlook.com>
2023-05-09 11:03:17 -0300

    uploader fix

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent, useState } from 'react';
-import * as XLSX from 'xlsx'
+import { useState } from 'react';
+import * as XLSX from 'xlsx';
 
 function FileUploader() {
   const [file, setFile] = useState(null);
+  const [sheetData, setSheetData] = useState([]);
 
-  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleFileUpload = (event) => {
     const uploadedFile = event.target.files[0];
     setFile(uploadedFile);
   };
@@ -25,7 +26,9 @@ function FileUploader() {
     reader.onload = (event) => {
       const data = event.target.result;
       const workbook = XLSX.read(data, { type: 'binary' });
-      console.log(workbook);
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const dataInSheet = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      setSheetData(dataInSheet);
     };
     reader.readAsBinaryString(file);
   };
@@ -38,9 +41,29 @@ function FileUploader() {
         onDragOver={(event) => event.preventDefault()}
         style={{ width: 200, height: 200, backgroundColor: 'gray' }}
       >
-        Arraste o arquivo aqui
+        Drop file here
       </div>
       <button onClick={handleFileSubmit}>Submit</button>
+      {sheetData.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {sheetData[0].map((cellData) => (
+                <th key={cellData}>{cellData}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {sheetData.slice(1).map((rowData, index) => (
+              <tr key={index}>
+                {rowData.map((cellData, index) => (
+                  <td key={index}>{cellData}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
